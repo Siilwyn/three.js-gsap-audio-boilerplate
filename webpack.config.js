5848c9b43c66648e106d48c09800af8d45f21d42
Selwyn <talk@selwyn.cc>
2018-05-20 16:55:37 +0200

    Update to Webpack 4 to simplify configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,20 @@
 const path = require('path');
 
 const config = {
-  // Input
-  entry: './src/main.js',
+  mode: process.env.NODE_ENV || 'production',
 
-  // Output
+  entry: './src/main.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
   },
 
-  // Transformations
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-          },
-        },
-      },
-    ],
-  },
-
-  // Source maps
   devtool: 'source-map',
-
-  // Server
   devServer: {
     contentBase: path.join(__dirname, 'src'),
     compress: true,
   },
 };
+
 module.exports = config;
